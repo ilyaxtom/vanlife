4d822866c24a7886acebfd8d6cbb45444d568a65
ilyaxtom <xtom38@gmail.com>
2024-07-21 20:25:49 +0300

    added host page

--- src/App.js
+++ src/App.js
@@ -4,22 +4,31 @@ import './App.css';
 import Home from './pages/Home';
 import About from "./pages/About";
 import Vans from "./pages/Vans";
+import VanDetail from "./pages/VanDetail";
+import Layout from "./components/Layout";
+import Dashboard from "./host/Dashboard";
+import Income from "./host/Income";
+import Reviews from "./host/Reviews";
+import HostLayout from "./components/HostLayout";
 
 function App() {
   return (
       <BrowserRouter>
-        <header>
-          <Link className="logo" to="/">#VanLife</Link>
-          <nav>
-            <Link to="/about">About</Link>
-            <Link to="/vans">Vans</Link>
-          </nav>
-        </header>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/vans" element={<Vans />} />
-        </Routes>
+          <Routes>
+              <Route path="/" element={<Layout />} >
+                  <Route index element={<Home />} />
+                  <Route path="about" element={<About />} />
+                  <Route path="vans" element={<Vans />} />
+                  <Route path="vans/:id" element={<VanDetail />} />
+
+
+                  <Route path="host" element={<HostLayout />}>
+                      <Route index element={<Dashboard />} />
+                      <Route path="income" element={<Income />} />
+                      <Route path="reviews" element={<Reviews />} />
+                  </Route>
+              </Route>
+          </Routes>
       </BrowserRouter>
   )
 }
